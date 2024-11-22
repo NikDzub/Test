d76fee71197c7475bf001cb79a18b0f731d41fd5
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-11-22 15:37:18 +0200

    Initial Commit

---
+++ TestTests/TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestTests.swift
+//  TestTests
+//
+//  Created by ihpt on 22/11/2024.
+//
+
+import Testing
+@testable import Test
+
+struct TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
